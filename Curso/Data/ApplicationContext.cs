1abb3550c3b60050b0b228239e12e6645267cb84
Henrique-Hoi-Dev <riqueah@gmail.com>
2020-10-01 17:02:50 -0300

    finished application

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using CursoEFCore.Domain;
 using Microsoft.Extensions.Logging;
+using System.Linq;
+using System;
 
 namespace CursoEFCore.Data
 {
@@ -17,11 +19,36 @@ namespace CursoEFCore.Data
         optionsBuilder
               .UseLoggerFactory(_logger)
               .EnableSensitiveDataLogging()
-              .UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=newTeste;Integrated Security=true");
+              .UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=newTeste;Integrated Security=true",
+              p=>p.EnableRetryOnFailure(
+              maxRetryCount: 2, 
+              maxRetryDelay: 
+              TimeSpan.FromSeconds(5),
+              errorNumbersToAdd: null)
+              .MigrationsHistoryTable("curso_ef_core")
+              );
       }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapearPropriedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+          foreach(var entity in modelBuilder.Model.GetEntityTypes())
+          {
+            var properteis = entity.GetProperties().Where(p=>p.ClrType == typeof(string));
+
+            foreach(var property in properteis)
+            {
+              if(string.IsNullOrEmpty(property.GetColumnType())
+                && !property.GetMaxLength().HasValue)
+                {
+                  property.SetColumnType("VARCHAR(100)");
+                }
+            }
+          }
         }
             
     }
